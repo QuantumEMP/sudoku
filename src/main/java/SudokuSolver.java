4d856adb72820d232c65f72dd3c0f09e27fb5090
QuantumEMP <jude1rose@outlook.com>
2025-11-20 14:05:36 +0200

    cleanrow added

--- src/main/java/SudokuSolver.java
+++ src/main/java/SudokuSolver.java
@@ -1,61 +1,58 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import logic.Cleanup;
 
-public class SudokuSolver {
-    static String inputFile;
-    static String outputFile;
+static String inputFile;
+static String outputFile;
 
-    public static void main(String[] args) {
+void main(String[] args) {
+    IO.println("start");
 
-        List<String> sudoku = readFile(args);
+    List<String> sudoku = readFile(args);
 
-        if  (Objects.equals(sudoku.getFirst(), "ERROR")){
-            System.exit(0);
-        }
-
-
+    if (Objects.equals(sudoku.getFirst(), "ERROR")) {
+        System.exit(0);
     }
 
-    private static List<String> readFile(String[] args) {
-        inputFile = args[0];
-        for (int i = 0; i <= args.length; i++){
-            if (Objects.equals(args[i], "-o")){
-                outputFile = args[i+1];
+    ArrayList<ArrayList<Object>> puzzle = convertLines(sudoku);
+    Cleanup.solve(puzzle);
+
+
+}
+
+private static List<String> readFile(String[] args) {
+    inputFile = args[0];
+    for (int i = 0; i < args.length; i++) {
+        if (Objects.equals(args[i], "-o")) {
+            outputFile = args[i + 1];
+        }
+    }
+
+    try {
+        return Files.readAllLines(Paths.get(inputFile));
+    } catch (IOException e) {
+        System.err.println("Error reading file: " + e.getMessage());
+    }
+    return List.of("ERROR");
+}
+
+private static ArrayList<ArrayList<Object>> convertLines(List<String> sudoku) {
+    ArrayList<ArrayList<Object>> result = new ArrayList<>();
+    for (String line : sudoku) {
+        String[] list = line.strip().split(" ");
+
+        ArrayList<Object> lineResult = new ArrayList<>();
+
+        for (String item : list) {
+            if (Objects.equals(item, "0")) {
+                item = "1,2,3,4,5,6,7,8,9";
+                lineResult.add(item);
+            } else {
+                lineResult.add(item);
             }
         }
 
-        try {
-            return Files.readAllLines(Paths.get(inputFile));
-        } catch (IOException e) {
-            System.err.println("Error reading file: " + e.getMessage());
-        }
-        return List.of("ERROR");
+        result.add(lineResult);
+
     }
 
-    private static ArrayList<ArrayList<String>> convertLines(List<String> sudoku){
-        ArrayList<ArrayList<String>> result = new ArrayList<>();
-        for (String line : sudoku){
-            String[] list = line.strip().split(" ");
-
-            ArrayList<String> lineResult = new ArrayList<>();
-
-            for (String item : list){
-                if (Objects.equals(item, "0")){
-                    item = "{1,2,3,4,5,6,7,8,9}";
-                    lineResult.add(item);
-                } else{
-                    lineResult.add(item);
-                }
-            }
-
-            result.add(lineResult);
-
-        }
-
-        return result;
-    }
+    return result;
 }
--- src/main/java/logic/Cleanup.java
+++ src/main/java/logic/Cleanup.java
@@ -1,6 +1,42 @@
 package logic;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Cleanup implements IMethods{
 
+    public static ArrayList<ArrayList<Object>> solve(ArrayList<ArrayList<Object>> puzzle){
+        puzzle = cleanRow(puzzle);
+        System.out.println(puzzle);
+        return null;
+    }
 
+    private static ArrayList<ArrayList<Object>> cleanRow(ArrayList<ArrayList<Object>> puzzle) {
+        ArrayList<ArrayList<Object>> r = new ArrayList<>();
+
+        for (ArrayList<Object> line : puzzle) {
+            ArrayList<Object> l = new ArrayList<>();
+            for (Object item : line) {
+                HashSet<Object> possibilities = new HashSet<>(Arrays.asList(item.toString().split(",")));
+                if (possibilities.size() == 1){
+                    l.add(item);
+                    continue;
+                }
+
+                StringBuilder result = new StringBuilder();
+                for (Object num : possibilities) {
+                    if (!line.contains(num)) {
+                        result.append(num).append(",");
+                    }
+                }
+
+                l.add(result.toString());
+            }
+            r.add(l);
+        }
+
+        return r;
+    }
 }
